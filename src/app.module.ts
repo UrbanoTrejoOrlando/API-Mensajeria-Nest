6e8e4e550cf442c11310beea99ce1bacb5f13b34
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-17 21:17:52 -0600

    Modificaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { UsersModule } from './users/users.module';
+import { ProductosModule } from './productos/productos.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Agregar mongodb a la api
+    MongooseModule.forRoot("mongodb://localhost:27017"),
+    UsersModule,
+    ProductosModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
