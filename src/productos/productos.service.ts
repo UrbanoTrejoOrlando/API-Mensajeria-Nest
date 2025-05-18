4ae6d2dfb27b4abc3ef231ac57bf703663f8b30d
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-17 21:21:40 -0600

    Porductos service

---
+++ src/productos/productos.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductoDto } from './dto/create-producto.dto';
+import { UpdateProductoDto } from './dto/update-producto.dto';
+import { Productos } from './schemas/producto.schema';
+
+@Injectable()
+export class ProductosService {
+  create(createProductoDto: CreateProductoDto) {
+    return new Productos();
+  }
+
+  findAll() {
+    return `This action returns all productos`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} producto`;
+  }
+
+  update(id: number, updateProductoDto: UpdateProductoDto) {
+    return `This action updates a #${id} producto`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} producto`;
+  }
+}
